2af20da2e413f3c33fcfce7cd8f21de181b7b112
Ta5r <srv.tanay@gmail.com>
2021-08-21 18:20:52 +0530

    create: userSchema model

--- server/index.js
+++ server/index.js
@@ -1,47 +1,40 @@
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-var express = require('express');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+var express = require("express");
 var app = express();
 
+dotenv.config({ path: "./config.env" });
 
-dotenv.config({path:"./config.env"});
+require("./db/conn");
+const User = require("./model/userSchema");
 
-const DB = process.env.DATABASE;
+const port = process.env.PORT;
 
-mongoose.connect(DB,{
-   useNewUrlParser: true,
-   useCreateIndex:true,
-   useUnifiedTopology: true,
-   useFindAndModify: false
-}).then(()=>{
-   console.log("connection successful");
-}).catch((err)=>{console.log(err)});
 //Midleware
 
-const middleware =(req,res,next)=>{
-   console.log("IN Middleware");
-   next();
-}
+const middleware = (req, res, next) => {
+  console.log("IN Middleware");
+  next();
+};
 
-app.get('/', (req, res) => {
-    res.send("<br>Epress - js");
- });
+app.get("/", (req, res) => {
+  res.send("<br>Epress - js");
+});
 
- app.get('/login', (req, res) => {
-    res.send("<br>LOGIN");
- });
+app.get("/login", (req, res) => {
+  res.send("<br>LOGIN");
+});
 
- app.get('/signup', (req, res) => {
-    res.send("<br>SIGNUP");
- });
-  
- app.get('/datasrc', (req, res) => {
-    res.send("<br>DATA SOURCE");
- });
+app.get("/signup", (req, res) => {
+  res.send("<br>SIGNUP");
+});
 
-app.get('/bots', (req, res) => {
-    res.send("<br>BOTS");
- });
- 
-  
-app.listen(8080);
+app.get("/datasrc", (req, res) => {
+  res.send("<br>DATA SOURCE");
+});
+
+app.get("/bots", (req, res) => {
+  res.send("<br>BOTS");
+});
+
+app.listen({ port });
